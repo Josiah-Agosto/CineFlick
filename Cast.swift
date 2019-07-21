4b7ecc06a04012fca91742a6209515b52f7bbd24
Josiah Agosto <Agosto_Josiah@yahoo.com>
2019-07-21 20:20:19 +0200

    Retrieves Movie Titles Only, for now.

---
+++ Cast.swift
@@ -0,0 +1,16 @@
+//
+//  Cast.swift
+//  CinemApp
+//
+//  Created by Josiah Agosto on 6/10/19.
+//  Copyright © 2019 Josiah Agosto. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+// Cast Members in the Movie
+struct Cast: Codable {
+    let actor: String
+//    let actorImage: UIImage
+}
